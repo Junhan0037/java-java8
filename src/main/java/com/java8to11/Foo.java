ef95a139c49dd3fc83cf7ccd4f5abb60319d9f24
Junhan <wnsgks0037@naver.com>
2020-12-11 16:36:34 +0900

    ADD : 자바에서 제공하는 함수형 인터페이스

--- src/main/java/com/java8to11/Foo.java
+++ src/main/java/com/java8to11/Foo.java
@@ -1,5 +1,7 @@
 package com.java8to11;
 
+import java.util.function.*;
+
 public class Foo {
     public static void main(String[] args) {
 
@@ -14,6 +16,28 @@ public class Foo {
         // 람다 표현식
         RunSomething runSomething2 = () -> System.out.println("Hello");
         runSomething2.doIt();
+
+        /*
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));
+         */
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        System.out.println(plus10.apply(1));
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2); // plus10함수 앞쪽에 multiply2과정을 합친다.
+        System.out.println(multiply2AndPlus10.apply(2));
+
+        Function<Integer, Integer> plus10AndMultiply2 = plus10.andThen(multiply2); // plus10함수 뒤쪽에 multiply2과정을 합친다.
+        System.out.println(plus10AndMultiply2.apply(2));
+
+        Consumer<Integer> printT = (i) -> System.out.println(i); // 반환타입이 없다.
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10; // 반환값을 지정.
+        System.out.println(get10.get());
+
+        Predicate<String> startsWithJunhan = (s) -> s.startsWith("junhan"); // boolean을 리턴
 
     }
 }
